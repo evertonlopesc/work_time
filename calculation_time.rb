31f5e94f7cd239db4bf3cb779e4b494314e3ce66
evertonlopesc <everton.lopess@outlook.com>
2022-06-26 14:43:14 -0300

    refactor call method.

--- calculation_time.rb
+++ calculation_time.rb
@@ -19,6 +19,12 @@ class CalculationTime
     @return_stop  = convert_time(@times[:return_stop])
   end
 
+  def call
+    puts "Begin time in   #{@entrance.strftime('%H:%M')} H"
+    puts "Time lunch:    #{calculate_lunch_break} H"
+    puts "Time stopped:  #{calculate_stopped_break} H"
+    puts "Total interval: #{calculate_range.strftime('%H:%M')} H"
+    puts "End of work in: #{calculate_end_work} H"
   end
 
   private
