19c6005ae98ff2f5c8fdca263294aea7e39cf91e
evertonlc <everton.lopess@outlook.com>
2023-02-24 20:13:51 -0300

    centralize data entries.

--- app.rb
+++ app.rb
@@ -1,36 +1,27 @@
 # frozen_string_literal: true
 
 require 'debug'
-require_relative 'lib/time_record'
-
-# This method records the working hours of users
-#
-# @param record [Object]
-#
-# @return working hours of user
-def your_times(record)
-  print 'Enter the job start time: '
-  record.register_entry(time: gets.chomp)
-
-  puts 'Want register lunch time? [ [y]es ] ou [ Enter ]'
-  print 'R: '
-  record.register_lunch(response: gets.chomp)
-
-  puts 'Want register break time? [ [y]es ] ou [ Enter ]'
-  print 'R: '
-  record.register_stop(response: gets.chomp)
-
-  system 'clear'
-
-  record.display_hours_worked
-end
+require_relative 'lib/time_record/record'
 
 # This method starting the application
 def init
   print 'Enter your username: '
-  time_record = Lib::TimeRecord.new(username: gets.chomp)
+  record = Lib::TimeRecord::Record.new(username: gets.chomp)
 
-  your_times(time_record)
+  print 'Enter the job start time: '
+  record.to_start
+
+  puts 'Want register lunch time? [ [y]es ] ou [ Enter ]'
+  print 'R: '
+  record.lunch_break(response: gets.chomp)
+
+  puts 'Want register break time? [ [y]es ] ou [ Enter ]'
+  print 'R: '
+  record.emergency_stop(response: gets.chomp)
+
+  system 'clear'
+
+  record.display_hours_worked
 end
 
 puts "APPLICATION TO CALCULATE YOUR WORKING HOURS\n\n"
